8f6496c8ccad31eb29858cce49876abdba09aefd
Alec Lauder <alauder98@gmail.com>
2021-12-01 21:53:00 +0000

    Day 1
    Solutions to day 1 puzzles

---
+++ Day1/Day1Puzzle1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] indvLines = text.Split('\n');
+
+                int increases = 0;
+
+                if (indvLines.Length > 0)
+                {
+                    int prev = int.Parse(indvLines[0]);
+
+                    for (int i = 1; i < indvLines.Length; i++)
+                    {
+                        if (!string.IsNullOrEmpty(indvLines[i]))
+                        {
+                            int current = int.Parse(indvLines[i]);
+
+                            if (current > prev)
+                            {
+                                increases++;
+                            }
+
+                            prev = current;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Total increases: " + increases);
+            }
+            else
+            {
+                Console.WriteLine("string is null!");
+            }
+        }
+    }
+}
---
+++ Day1/Day1Puzzle2.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOC
+{
+    class Window
+    {
+        int[] values;
+        int index;
+
+        public Window(int valLength)
+        {
+            values = new int[valLength];
+            index = 0;
+        }
+
+        public void AddValue(int val)
+        {
+            values[index] = val;
+            index++;
+        }
+
+        public bool ReadyToGo()
+        {
+            return index == values.Length;
+        }
+
+        public int GetValue()
+        {
+            int returnVal = 0;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                returnVal += values[i];
+            }
+
+            return returnVal;
+        }
+    }
+
+    class Tracker
+    {
+        List<Window> windows;
+        List<Window> finishedWindows;
+
+        public Tracker()
+        {
+            windows = new List<Window>();
+            finishedWindows = new List<Window>();
+        }
+
+        void AddWindow()
+        {
+            windows.Add(new Window(3));
+        }
+
+        public void AddValue(int val)
+        {
+            AddWindow();
+
+            for (int i = 0; i < windows.Count; i++)
+            {
+                windows[i].AddValue(val);
+            }
+
+            CheckWindows();
+        }
+
+        void CheckWindows()
+        {
+            for (int i = 0; i < windows.Count; i++)
+            {
+                if (windows[i].ReadyToGo())
+                {
+                    finishedWindows.Add(windows[i]);
+                    windows.RemoveAt(i);
+                }
+            }
+        }
+
+        public int CheckDifs()
+        {
+            int increases = 0;
+
+            int prev = finishedWindows[0].GetValue();
+
+            for (int i = 0; i < finishedWindows.Count; i++)
+            {
+                int current = finishedWindows[i].GetValue();
+
+                if (current > prev)
+                {
+                    increases++;
+                }
+
+                prev = current;
+            }
+
+            return increases;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] indvLines = text.Split('\n');
+
+                if (indvLines.Length > 0)
+                {
+                    Tracker track = new Tracker();
+
+                    for (int i = 0; i < indvLines.Length; i++)
+                    {
+                        if (!string.IsNullOrEmpty(indvLines[i]))
+                        {
+                            track.AddValue(int.Parse(indvLines[i]));
+                        }
+                    }
+
+                    Console.WriteLine("Total increases: " + track.CheckDifs());
+                }
+            }
+            else
+            {
+                Console.WriteLine("string is null!");
+            }
+        }
+    }
+}
