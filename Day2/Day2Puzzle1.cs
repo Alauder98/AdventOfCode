5805507d4e37dc6286fb5be8a279470ed5f45e89
Alec Lauder <alauder98@gmail.com>
2021-12-02 20:57:19 +0000

    Day2 puzzles

---
+++ Day2/Day2Puzzle1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOC
+{
+    class Program
+    {
+        const string FORWARD = "forward";
+        const string DOWN = "down";
+        const string UP = "up";
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] indvLines = text.Split('\n');
+
+                if (indvLines.Length > 0)
+                {
+                    int horizontal = 0;
+                    int depth = 0;
+
+                    for (int i = 0; i < indvLines.Length; i++)
+                    {
+                        if (!string.IsNullOrEmpty(indvLines[i]))
+                        {
+                            string[] vals = indvLines[i].Split(' ');
+
+                            int modifier = int.Parse(vals[1]);
+
+                            switch (vals[0])
+                            {
+                                case FORWARD:
+                                    horizontal += modifier;
+                                    break;
+                                case DOWN:
+                                    depth += modifier;
+                                    break;
+                                case UP:
+                                    depth -= modifier;
+                                    break;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("End result: " + (horizontal * depth));
+                }
+            }
+            else
+            {
+                Console.WriteLine("string is null!");
+            }
+        }
+    }
+}
---
+++ Day2/Day2Puzzle2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOC
+{
+    class Program
+    {
+        const string FORWARD = "forward";
+        const string DOWN = "down";
+        const string UP = "up";
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] indvLines = text.Split('\n');
+
+                if (indvLines.Length > 0)
+                {
+                    int horizontal = 0;
+                    int depth = 0;
+                    int aim = 0;
+
+                    for (int i = 0; i < indvLines.Length; i++)
+                    {
+                        if (!string.IsNullOrEmpty(indvLines[i]))
+                        {
+                            string[] vals = indvLines[i].Split(' ');
+
+                            int modifier = int.Parse(vals[1]);
+
+                            switch (vals[0])
+                            {
+                                case FORWARD:
+                                    horizontal += modifier;
+                                    depth += (aim * modifier);
+                                    break;
+                                case DOWN:
+                                    aim += modifier;
+                                    break;
+                                case UP:
+                                    aim -= modifier;
+                                    break;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("End result: " + (horizontal * depth));
+                }
+            }
+            else
+            {
+                Console.WriteLine("string is null!");
+            }
+        }
+    }
+}
