db8d7c16daab63c5ec707f3bb8128e6e0084699b
Alec Lauder <alauder98@gmail.com>
2021-12-07 21:40:27 +0000

    Day 7
    Wish I could find a better algo for finding the lowest on part 2

---
+++ Day7/Day7Puzzle1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC
+{
+    class Program
+    {
+        static int GetFuel(int[] arr, int start)
+        {
+            int totalCost = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                totalCost += Math.Abs((arr[i] - start));
+            }
+
+            return totalCost;
+        }
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] indvLines = text.Split(',');
+
+                if (indvLines.Length > 0)
+                {
+                    
+                    List<int> values = new List<int>();
+
+                    for (int i = 0; i < indvLines.Length; i++)
+                    {
+                        values.Add(int.Parse(indvLines[i]));
+                    }
+
+                    values.Sort();
+
+                    int middle = values.Count / 2;
+
+                    Console.WriteLine("Min is: " + Math.Min(Math.Min(GetFuel(values.ToArray(), values[middle]),
+                       GetFuel(values.ToArray(), values[middle - 1])), GetFuel(values.ToArray(), values[middle + 1])));
+                }
+            }
+            else
+            {
+                Console.WriteLine("string is null!");
+            }
+        }
+    }
+}
---
+++ Day7/Day7Puzzle2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC
+{
+    class Program
+    {
+        static int GetFuel(int[] arr, int start)
+        {
+            int totalCost = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                totalCost += TriangleNumber(Math.Abs((arr[i] - start)));
+            }
+
+            return totalCost;
+        }
+
+        static int TriangleNumber(int n)
+        {
+            if (n == 0) { return 0; }
+            return (n * (n + 1)) / 2; 
+        }
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] indvLines = text.Split(',');
+
+                if (indvLines.Length > 0)
+                {
+                    
+                    List<int> values = new List<int>();
+
+                    for (int i = 0; i < indvLines.Length; i++)
+                    {
+                        values.Add(int.Parse(indvLines[i]));
+                    }
+
+                    int lowest = GetFuel(values.ToArray(), values[0]);
+                    for (int i = 1; i < values.Count; i++)
+                    {
+                        int gottenValue = GetFuel(values.ToArray(), values[i]);
+
+                        if (lowest > gottenValue)
+                        {
+                            lowest = gottenValue;
+                        }
+                    }
+
+                    Console.WriteLine("Lowest is now: " + lowest);
+                }
+            }
+            else
+            {
+                Console.WriteLine("string is null!");
+            }
+        }
+    }
+}
